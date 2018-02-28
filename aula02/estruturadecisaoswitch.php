fc8994c0df19f2885da9f55a62718d92765f42a7
Larissa Fujiwara <fujiwara.larissa@gmail.com>
2018-02-27 22:12:50 -0300

    arquivos

---
+++ aula02/estruturadecisaoswitch.php
@@ -0,0 +1,15 @@
+<?php
+
+$nome = readline ("digite seu nome:");
+
+switch ($nome) {
+	case 'Lucas':
+		echo "Oi Lucas";
+		break;
+	case 'Larissa':
+		echo "Oi Larissa";
+		break;
+	case 'Breno':
+		echo "Oi Breno";
+		break;
+}
