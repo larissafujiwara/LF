6aed54f4699e4b51a17e5d109b272387834cda44
Larissa Fujiwara <fujiwara.larissa@gmail.com>
2018-02-27 22:08:00 -0300

    arquivos

---
+++ aula02/exerciciodecisao.php
@@ -0,0 +1,16 @@
+<?php
+
+//Se o numero dividido por 2 e resto da divisão = 0
+//modulo % - retorna o resto da divisão
+
+echo "<pre>";
+
+$num = 5;
+
+if ($num % 2 === 0) {
+	echo "Par";
+}
+
+else {
+	echo "Impar";
+}
